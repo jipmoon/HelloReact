9d46250c99c733ab2694cac76417d14a247f3277
jipmoon <hyungjip@Hyungs-MacBook-Pro-2.local>
2016-12-15 12:27:24 -0800

    Added component properties on jsx

--- public/app.jsx
+++ public/app.jsx
@@ -2,17 +2,31 @@
    2. Create a variable Greeter then use one function
    3. createClass takes one argument that is an option's object
    4. Define the behaviour for our component*/
-var Greeter = React.createClass();
+var Greeter = React.createClass({
+  // Provide default value for the name props
+  // return the object of property
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'YOLO'
+    };
+  },
   render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
     return (
       <div>
-        <h1>Hello React!</h1>
-        <p>This is a form a component</p>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
       </div>
     );
   }
 });
+
+var firstName = 'Jip';
+var myMessage = 'This is a message';
 ReactDOM.render(
-  <Greeter/>,
+  // Passing the property
+  <Greeter name={firstName} message = {myMessage} />,
   document.getElementById('app')
 );
